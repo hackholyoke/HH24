dcf3f9aa78dc9e95512a8cce1a381ee5ac2e4fb3
yuwatidora <yuwatinyi@n236-88.mtholyoke.edu>
2024-08-31 02:29:27 -0400

    Remove .next/ from version control

--- .next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VeA6u5zuUhtAfVj3n0ddpvr5z1T5ArULDo7JJilvWes=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"frSrUsVTt+yj6tYWMUHcjcSr0SRSh3fv+lh763ukBf4=\"\n}"
->>>>>>> 5950b787b82ae4a58db8c0864bd0c91a704a9b41
