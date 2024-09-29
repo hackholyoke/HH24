fe96a7bda00b202acfa50e51fd91f22db7b3bc3e
yuwatidora <yuwatinyi@n237-27.mtholyoke.edu>
2024-09-28 22:30:09 -0400

    challenges responsive, challenges dot buttons, remove schedule and speaker from menu, process of adding bg to team pages

--- app/components/Menu/menuBar.js
+++ app/components/Menu/menuBar.js
@@ -21,9 +21,9 @@ const MenuBar = () => {
 
         <a onClick={() => handleClick('/')} className='item' style={{ cursor: 'pointer' }}>Home</a>
         <a onClick={() => handleClick('/#about')} href='#about' className='item'>About</a>
-        <a onClick={() => handleClick('/#schedule')} href='#schedule' className='item'>Schedule</a>
+        {/* <a onClick={() => handleClick('/#schedule')} href='#schedule' className='item'>Schedule</a> */}
         <a onClick={() => handleClick('/#challenges')}  href='#challenges' className='item'>Challenges</a>
-        <a onClick={() => handleClick('/#speakers')} href='#speakers' className='item'>Speakers</a>
+        {/* <a onClick={() => handleClick('/#speakers')} href='#speakers' className='item'>Speakers</a> */}
         <a onClick={() => handleClick('/#sponsors')} href='#sponsors' className='item'>Sponsors</a>
         <a onClick={() => handleClick('/#faq')} href='#faq' className='item'>FAQ</a>
         <a onClick={() => handleClick('/team_page')}  className='item' style={{ cursor: 'pointer' }}>Meet the team</a>
