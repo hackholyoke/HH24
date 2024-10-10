283748b09c2d4d2de74dc9bb2b78469af5df9832
iam-zoey <0331912@gmail.com>
2024-10-10 14:48:19 -0400

    update registeration link

--- app/components/Menu/menuBar.js
+++ app/components/Menu/menuBar.js
@@ -12,25 +12,25 @@ const MenuBar = () => {
 
   const handleRegisterClick = () => {
     setTimeout(() => {
-      window.open('https://forms.gle/LiPSTWoEuJGuLpMz9', '_blank');
-  }, 300)
+      window.open('https://forms.gle/xXXddv89HPbujm7g7', '_blank');
+    }, 300)
   }
 
   return (
-        <div className="menuBar">
+    <div className="menuBar">
 
-        <a onClick={() => handleClick('/')} className='item' style={{ cursor: 'pointer' }}>Home</a>
-        <a onClick={() => handleClick('/#about')} href='#about' className='item'>About</a>
-        <a onClick={() => handleClick('/#schedule')} href='#schedule' className='item'>Schedule</a>
-        <a onClick={() => handleClick('/#challenges')}  href='#challenges' className='item'>Challenges</a>
-        <a onClick={() => handleClick('/#speakers')} href='#speakers' className='item'>Speakers</a>
-        <a onClick={() => handleClick('/#sponsors')} href='#sponsors' className='item'>Sponsors</a>
-        <a onClick={() => handleClick('/#faq')} href='#faq' className='item'>FAQ</a>
-        <a onClick={() => handleClick('/team_page')}  className='item' style={{ cursor: 'pointer' }}>Meet the team</a>
-        <a onClick={() => handleRegisterClick()} className='item'>Register</a>
+      <a onClick={() => handleClick('/')} className='item' style={{ cursor: 'pointer' }}>Home</a>
+      <a onClick={() => handleClick('/#about')} href='#about' className='item'>About</a>
+      <a onClick={() => handleClick('/#schedule')} href='#schedule' className='item'>Schedule</a>
+      <a onClick={() => handleClick('/#challenges')} href='#challenges' className='item'>Challenges</a>
+      <a onClick={() => handleClick('/#speakers')} href='#speakers' className='item'>Speakers</a>
+      <a onClick={() => handleClick('/#sponsors')} href='#sponsors' className='item'>Sponsors</a>
+      <a onClick={() => handleClick('/#faq')} href='#faq' className='item'>FAQ</a>
+      <a onClick={() => handleClick('/team_page')} className='item' style={{ cursor: 'pointer' }}>Meet the team</a>
+      <a onClick={() => handleRegisterClick()} className='item'>Register</a>
 
-      </div>
-   
+    </div>
+
   );
 };
 
