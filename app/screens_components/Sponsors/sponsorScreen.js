fabde92eb7d36bf1ddaa85be4fbecc4093d2a4ce
yuwatidora <yuwatinyi@n247-43.mtholyoke.edu>
2024-10-21 17:08:51 -0400

    CSS fixes

--- app/screens_components/Sponsors/sponsorScreen.js
+++ app/screens_components/Sponsors/sponsorScreen.js
@@ -8,12 +8,12 @@ import mhc_cs from "../../assets/sponsors/mhc_cs.png"
 const SponsorScreen = () =>{
 
     return (
-        <div>
+        <div className="sponsorScreen">
             <h1 className = "sponsor-heading">Sponsors</h1>
             <div className = "spnsr-images">
-                <Image src={wolframalpha}/>
-                <Image src={mhc_cs}/>
-
+                <Image src={mhc_cs} className="mhc_cs_logo"/>
+                <Image src={wolframalpha} className="wolfram_logo"/>
+                
             </div>
             <p className='spn-desc'>If you are interested in contributing as a sponsor or mentor please click below.</p>
             <a href="mailto:hackholyoke@gmail.com" className="link">
