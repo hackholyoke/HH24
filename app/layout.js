0948b6e14e6b0228a64ec82a6ceece04fa364b28
yuwatidora <yuwatinyi003@gmail.com>
2024-10-31 17:16:35 -0400

    css fixes for safari

--- app/layout.js
+++ app/layout.js
@@ -27,10 +27,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name="google-site-verification" content="Hx0XNhaS4BBCzzF5fp6ywmIxuuypBjHqsyDli3N2wqM" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
         <link rel="icon" href={metadata.icons.icon[0].url} sizes={metadata.icons.icon[0].sizes} type={metadata.icons.icon[0].type} />
         <title>HackHolyoke 2024</title>
+        <meta name="google-site-verification" content="Hx0XNhaS4BBCzzF5fp6ywmIxuuypBjHqsyDli3N2wqM" />
       </Head>
       <body style={{backgroundImage: 'linear-gradient(to bottom, black , #2c3c94 , black, #2c3c94 , black)'
 }}>{children}
