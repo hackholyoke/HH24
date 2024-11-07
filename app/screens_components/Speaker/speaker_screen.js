43b620833676157ed23ad4b2c13ded0994941cbf
yuwatidora <yuwatinyi003@gmail.com>
2024-11-06 19:17:15 -0500

    breakpoint and ariel

--- app/screens_components/Speaker/speaker_screen.js
+++ app/screens_components/Speaker/speaker_screen.js
@@ -13,7 +13,7 @@ import Mai from "../../assets/speakers/Mai.jpg"
 import Olivia from "../../assets/speakers/Olivia.jpg"
 import Ziyin from "../../assets/speakers/Ziyin.JPG"
 import noprofile from "../../assets/speakers/noprofile.png"
-import noprofilet from "../../assets/speakers/noprofilet.png"
+import breakpoint from "../../assets/speakers/breakpoint.JPG"
 
 
 const speakerCards=[
@@ -53,7 +53,7 @@ const speakerCards=[
   />,
 
   <SpeakerCard
-  speakerPic={noprofile}
+  speakerPic={breakpoint}
   speakerName={ "Nafisa Bashar Anmul, Kathryn Spencer, Sadichchha Maharjan  "}
   speakerPosition={"Breakpoint"}
   speakerDesc={"Have you ever wondered how all the face filters work on Snapchat and Instagram? Join us for a hands-on workshop where we’ll combine computer vision and creative coding using Mediapipe ML libraries in p5.js! You’ll learn to use hand and pose tracking to build interactive projects that respond to real-time gestures and face movements. Whether you're new to coding or experienced, this workshop will show you how to create your own immersive applications. Get ready to dive into creative coding and see how fun and accessible working with computer vision can be!"}
@@ -74,7 +74,7 @@ const speakerCards=[
   speakerPic={Ariel}
   speakerName={"Ariel Russ"}
   speakerPosition={"Senior Manager of Sustainability at Estée Lauder"}
-  speakerDesc={"COMING SOON !"}
+  speakerDesc={"Both informational interviews and traditional job-hunting ones are scary but I'm here to make it easier for you. You'll learn some skills to prep in advance and make you more confident."}
   speakerTitle={"Interviews, Networking and Managing Your Online Presence: LinkedIn and Beyond"}
   />,
 
