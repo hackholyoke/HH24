3ace1a810b4503f7ffbe94560cbd84c1e1ab38ec
yuwatidora <yuwatinyi003@gmail.com>
2024-11-07 13:39:02 -0500

    registration Form addition

---
+++ app/registration/page.js
@@ -0,0 +1,7 @@
+"use client"
+import './registration.css'
+import React, { useState } from "react";
+
+export default function Page() {
+    return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+  }
